eeb235ef3aa338bd1a20b73ac921ff3748d5e2e1
Yoni Satat <yoni.satat@gmail.com>
2017-10-16 17:40:43 +0100

    added home page and connect it to controller

--- rock_scissor_paper/controller.rb
+++ rock_scissor_paper/controller.rb
@@ -2,7 +2,9 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('./models/game.rb')
 
-
+get '/' do
+  erb(:home)
+end
 
 get '/:player1/:player2' do
   game = Game.new(params[:player1], params[:player2])
